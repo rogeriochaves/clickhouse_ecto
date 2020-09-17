64cbe3fbb7bbd7b33ec9b8330aa90501c58f0b0b
Rogerio Chaves <rogeriocfj@gmail.com>
2020-09-17 22:36:50 +0200

    Use latest clickhousex

--- mix.exs
+++ mix.exs
@@ -24,10 +24,10 @@ defmodule ClickhouseEcto.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ecto_sql, "~> 3.2"},
-      {:clickhousex, git: "https://github.com/lanalabs/clickhousex.git", tag: "v0.2.5"},
+      {:ecto_sql, "~> 3.0"},
+      {:clickhousex, "~> 0.4.0"},
       {:ex_doc, "~> 0.21", only: :dev},
-      {:db_connection, "~> 2.2"}
+      {:db_connection, "~> 2.0.0"}
     ]
   end
 
